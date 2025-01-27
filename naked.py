ab46c26b91e23fec2e59fcd8d089c93645a9f894
lanak <lana.kaimina@tvnet.lv>
2025-01-27 08:24:50 +0200

    Tika ieviesta zurnalesana.

--- naked.py
+++ naked.py
@@ -4,14 +4,26 @@ import json
 import datetime
 import time
 import yaml
+import logging
+import logging.config
+import yaml
+
+# Loading logging configuration
+	with open('./log_worker.yaml', 'r') as stream:
+	    log_config = yaml.safe_load(stream)
+
+	logging.config.dictConfig(log_config)
+
+	# Creating logger
+	logger = logging.getLogger('root')
 
 # Tiek importets paredzama notikuma laiks.
 from datetime import datetime
-print('Asteroid processing service')
+logger.info('Asteroid processing service')
 from configparser import ConfigParser
 
 # Initiating and reading config values
-print('Loading configuration from file')
+logger.info('Loading configuration from file')
 
 # 
 
@@ -24,21 +36,23 @@ try:
 
 	except:
 		logger.exception('')
-	print('DONE')
+	logger.info('DONE')
+
+
 
 
 # Getting todays date
 dt = datetime.now()
 request_date = str(dt.year) + "-" + str(dt.month).zfill(2) + "-" + str(dt.day).zfill(2)  
-print("Generated today's date: " + str(request_date))
+logger.debug("Generated today's date: " + str(request_date))
 
 # Uz ekrana tiek izvaditi pieprasijuma parametri.
-print("Request url: " + str(nasa_api_url + "rest/v1/feed?start_date=" + request_date + "&end_date=" + request_date + "&api_key=" + nasa_api_key))
+logger.info("Request url: " + str(nasa_api_url + "rest/v1/feed?start_date=" + request_date + "&end_date=" + request_date + "&api_key=" + nasa_api_key))
 r = requests.get(nasa_api_url + "rest/v1/feed?start_date=" + request_date + "&end_date=" + request_date + "&api_key=" + nasa_api_key)
 
-print("Response status code: " + str(r.status_code))
-print("Response headers: " + str(r.headers))
-print("Response content: " + str(r.text))
+logger.debug("Response status code: " + str(r.status_code))
+logger.debug("Response headers: " + str(r.headers))
+logger.debug("Response content: " + str(r.text))
 
 # Asteroidi tiek klasificeti
 if r.status_code == 200:
@@ -51,7 +65,7 @@ if r.status_code == 200:
     # Tiek saskaititi visi asteroidi
 	if 'element_count' in json_data:
 		ast_count = int(json_data['element_count'])
-		print("Asteroid count today: " + str(ast_count))
+		logger.info("Asteroid count today: " + str(ast_count))
 
         # Tiek izskaitlota asteroida bistamibas pakape.
 		if ast_count > 0:
@@ -93,7 +107,7 @@ if r.status_code == 200:
 							tmp_ast_close_appr_dt_utc = "1969-12-31 23:59:59"
 							tmp_ast_close_appr_dt = "1969-12-31 23:59:59"
 					else:
-						print("No close approach data in message")
+						logger.info("No close approach data in message")
 						tmp_ast_close_appr_ts = 0
 						tmp_ast_close_appr_dt_utc = "1970-01-01 00:00:00"
 						tmp_ast_close_appr_dt = "1970-01-01 00:00:00"
@@ -101,10 +115,10 @@ if r.status_code == 200:
 						tmp_ast_miss_dist = -1
 
                     # Tiek identificets konkrets asteroids un ta pietuvosanas Zemei
-					print("------------------------------------------------------- >>")
-					print("Asteroid name: " + str(tmp_ast_name) + " | INFO: " + str(tmp_ast_nasa_jpl_url) + " | Diameter: " + str(tmp_ast_diam_min) + " - " + str(tmp_ast_diam_max) + " km | Hazardous: " + str(tmp_ast_hazardous))
-					print("Close approach TS: " + str(tmp_ast_close_appr_ts) + " | Date/time UTC TZ: " + str(tmp_ast_close_appr_dt_utc) + " | Local TZ: " + str(tmp_ast_close_appr_dt))
-					print("Speed: " + str(tmp_ast_speed) + " km/h" + " | MISS distance: " + str(tmp_ast_miss_dist) + " km")
+					logger.info("------------------------------------------------------- >>")
+					logger.info("Asteroid name: " + str(tmp_ast_name) + " | INFO: " + str(tmp_ast_nasa_jpl_url) + " | Diameter: " + str(tmp_ast_diam_min) + " - " + str(tmp_ast_diam_max) + " km | Hazardous: " + str(tmp_ast_hazardous))
+					logger.info("Close approach TS: " + str(tmp_ast_close_appr_ts) + " | Date/time UTC TZ: " + str(tmp_ast_close_appr_dt_utc) + " | Local TZ: " + str(tmp_ast_close_appr_dt))
+					logger.info("Speed: " + str(tmp_ast_speed) + " km/h" + " | MISS distance: " + str(tmp_ast_miss_dist) + " km")
 					
 					# Adding asteroid data to the corresponding array
 					if tmp_ast_hazardous == True:
@@ -113,23 +127,23 @@ if r.status_code == 200:
 						ast_safe.append([tmp_ast_name, tmp_ast_nasa_jpl_url, tmp_ast_diam_min, tmp_ast_diam_max, tmp_ast_close_appr_ts, tmp_ast_close_appr_dt_utc, tmp_ast_close_appr_dt, tmp_ast_speed, tmp_ast_miss_dist])
 
 		else:
-			print("No asteroids are going to hit earth today")
+			logger.info("No asteroids are going to hit earth today")
 
     # Pec iegutajiem datiem tiek izveidots kopsavilkums par asteroidiem, kas sodien pietuvosies Zemei
-	print("Hazardous asteorids: " + str(len(ast_hazardous)) + " | Safe asteroids: " + str(len(ast_safe)))
+	logger.info("Hazardous asteorids: " + str(len(ast_hazardous)) + " | Safe asteroids: " + str(len(ast_safe)))
 
 	if len(ast_hazardous) > 0:
 
 		ast_hazardous.sort(key = lambda x: x[4], reverse=False)
 
-		print("Today's possible apocalypse (asteroid impact on earth) times:")
+		logger.info("Today's possible apocalypse (asteroid impact on earth) times:")
 		for asteroid in ast_hazardous:
-			print(str(asteroid[6]) + " " + str(asteroid[0]) + " " + " | more info: " + str(asteroid[1]))
+			logger.info(str(asteroid[6]) + " " + str(asteroid[0]) + " " + " | more info: " + str(asteroid[1]))
 
 		ast_hazardous.sort(key = lambda x: x[8], reverse=False)
-		print("Closest passing distance is for: " + str(ast_hazardous[0][0]) + " at: " + str(int(ast_hazardous[0][8])) + " km | more info: " + str(ast_hazardous[0][1]))
+		logger.info("Closest passing distance is for: " + str(ast_hazardous[0][0]) + " at: " + str(int(ast_hazardous[0][8])) + " km | more info: " + str(ast_hazardous[0][1]))
 	else:
-		print("No asteroids close passing earth today")
+		logger.info("No asteroids close passing earth today")
 
 else:
-	print("Unable to get response from API. Response code: " + str(r.status_code) + " | content: " + str(r.text))
+	logger.error("Unable to get response from API. Response code: " + str(r.status_code) + " | content: " + str(r.text))
